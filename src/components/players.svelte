9f872df5d0a6e99cc1481b347d47fd55c47fdd9d
malaak <teenaomalak@gmail.com>
2024-08-11 22:45:37 +0300

    move players to their own component

---
+++ src/components/players.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getWidthPercentage } from '../lib/utils';
+
+	let { playerData, top_Damage } = $props();
+</script>
+
+<div class="flex flex-row items-center justify-between bg-sGreenDark9 px-1 text-sGreenPrimary">
+	<p class="w-[15ch] grow flex-nowrap overflow-x-hidden text-nowrap">
+		{`${playerData.job.toUpperCase()} ${playerData.name}`}
+	</p>
+	<span class="shrink-0 ps-1 text-xs">{`${playerData.dps}[${playerData.critHitPct}]`}</span>
+</div>
+<span
+	class="h-[1px] bg-sGreenPrimary"
+	style={`width: ${getWidthPercentage(playerData.damage, top_Damage)}%;`}
+></span>
--- src/routes/encounter.svelte
+++ src/routes/encounter.svelte
@@ -1,35 +1,24 @@
 <script>
+	import Players from '../components/players.svelte';
 	import { getOverlayData } from '../lib/encounter.svelte';
-	import { getWidthPercentage } from '../lib/utils';
 
 	let overlayData = getOverlayData();
-	let players = $derived(overlayData.combatants);
 	let topDamage = $derived(overlayData.combatants[0].damage);
 </script>
 
 <div class="bg-primarybg flex w-full flex-col text-sm">
 	<div
-		class="flex flex-row items-center justify-between bg-sGreenPrimary px-1 font-semibold text-sGreenDark"
+		class="flex flex-row items-center justify-between bg-sGreenPrimary px-1 pt-[1px] font-semibold text-sGreenDark"
 	>
-		<h1>{overlayData.encounter.zoneName}</h1>
+		<h2 class="ps-1 text-[12px]">{overlayData.encounter.zoneName}</h2>
+
 		<p class="text-xs font-bold">{overlayData.encounter.duration}</p>
 	</div>
 	<span class="h-[1px]"></span>
 
 	<div class="flex flex-col pb-1">
-		{#each players as player}
-			<div
-				class="flex flex-row items-center justify-between bg-sGreenDark9 px-1 text-sGreenPrimary"
-			>
-				<p class="w-[15ch] grow flex-nowrap overflow-x-hidden text-nowrap">
-					{`${player.job.toUpperCase()} ${player.name}`}
-				</p>
-				<span class="shrink-0 ps-1 text-xs">{`${player.dps}[${player.critHitPct}]`}</span>
-			</div>
-			<span
-				class="h-[1px] bg-sGreenPrimary"
-				style={`width: ${getWidthPercentage(player.damage, topDamage)}%;`}
-			></span>
+		{#each overlayData.combatants as player}
+			<Players playerData={player} top_Damage={topDamage} />
 		{/each}
 	</div>
 </div>

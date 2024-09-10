c534a1eca5031419c0c4b73ee3e5a6003c9eab32
malaak <teenaomalak@gmail.com>
2024-09-10 16:44:01 +0300

    adjust red dps color to be more prominent.

--- src/lib/changeLog.js
+++ src/lib/changeLog.js
@@ -1,5 +1,9 @@
 const changeLog = [
 	{
+		version: '0.1.6',
+		changes: ['+ Added Role Color option.']
+	},
+	{
 		version: '0.1.5',
 		changes: [
 			'* Replaced Encounter total damage with deaths.',
@@ -9,11 +13,11 @@ const changeLog = [
 	{
 		version: '0.1.4',
 		changes: ['+ Added a few different ways to display player name.']
-	},
-	{
-		version: '0.1.3',
-		changes: ['* Small adjustments to the Settings UI.']
 	}
+	// {
+	// 	version: '0.1.3',
+	// 	changes: ['* Small adjustments to the Settings UI.']
+	// }
 	// {
 	// 	version: '0.1.2',
 	// 	changes: ['+ Added a different way to display player dps.']
